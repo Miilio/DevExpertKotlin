cec73a3027e3fd69052deed5b9c6b84e9baf34ca
Emilio Higueras <emhimu@gmail.com>
2023-03-19 13:33:14 +0100

    Exercise 1 with list

--- src/jvmMain/kotlin/Note.kt
+++ src/jvmMain/kotlin/Note.kt
@@ -1,1 +1,14 @@
 data class Note(val title: String, val description: String)
+
+val list = listOf(
+    Note("title 1", "description 1"),
+    Note("title 2", "description 2"),
+    Note("title 3", "description 3"),
+    Note("title 4", "description 4"),
+    Note("title 5", "description 5"),
+    Note("title 6", "description 6"),
+    Note("title 7", "description 7"),
+    Note("title 8", "description 8"),
+    Note("title 9", "description 9"),
+    Note("title 10", "description 10"),
+)
