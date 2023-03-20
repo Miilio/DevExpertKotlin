7a455d51fe0c670c96d6e78882b188d3316262e2
Emilio Higueras <emhimu@gmail.com>
2023-03-20 20:25:12 +0100

    Exercise 1 with enum

--- src/jvmMain/kotlin/Note.kt
+++ src/jvmMain/kotlin/Note.kt
@@ -1,14 +1,19 @@
-data class Note(val title: String, val description: String)
+data class Note(val title: String, val description: String, val type: Type) {
+    enum class Type {
+        TEXT, AUDIO
+    }
+}
 
 val list = listOf(
-    Note("title 1", "description 1"),
-    Note("title 2", "description 2"),
-    Note("title 3", "description 3"),
-    Note("title 4", "description 4"),
-    Note("title 5", "description 5"),
-    Note("title 6", "description 6"),
-    Note("title 7", "description 7"),
-    Note("title 8", "description 8"),
-    Note("title 9", "description 9"),
-    Note("title 10", "description 10"),
+    Note("title 1", "description 1", Note.Type.TEXT),
+    Note("title 2", "description 2", Note.Type.AUDIO),
+    Note("title 3", "description 3", Note.Type.TEXT),
+    Note("title 4", "description 4", Note.Type.TEXT),
+    Note("title 5", "description 5", Note.Type.TEXT),
+    Note("title 6", "description 6", Note.Type.AUDIO),
+    Note("title 7", "description 7", Note.Type.TEXT),
+    Note("title 8", "description 8", Note.Type.TEXT),
+    Note("title 9", "description 9", Note.Type.TEXT),
+    Note("title 10", "description 10", Note.Type.AUDIO),
 )
+
